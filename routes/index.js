aae43ce09c6c8edd46030c70a858a660980c7644
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-09-03 18:02:52 +0000

    UsersController.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
@@ -13,6 +15,22 @@ const routeController = (app) => {
 
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 };
 
